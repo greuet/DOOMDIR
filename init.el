aea38bc1fdf5a2ba01283260c960edfa1401ed58
Aurélien Greuet <aureliengreuet@gmail.com>
2022-09-12 15:15:17 +0200

    fix typos in init.el

--- init.el
+++ init.el
@@ -87,7 +87,7 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
-       (debugger+lsp)      ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)            ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
@@ -157,7 +157,7 @@
        php                 ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python + ls)       ; beautiful is better than ugly
+       (python + lsp)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
